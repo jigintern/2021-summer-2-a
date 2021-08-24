3bc9ef8ddc63239737f881a0a11ddf9671eb5b36
okomechen <tomoaki.1023.8686@gmail.com>
2021-08-24 13:31:26 +0900

    create top button  scripts

---
+++ static/screen/src/top_scripts.js
@@ -0,0 +1,10 @@
+const start=document.getElementById("start");
+start.addEventListener("click",function(){
+    //ここにstartボタンのコードをかく
+    console.log("hogehoge");
+});
+
+const option=document.getElementById("option");
+start.addEventListener("click",function(){
+    //ここにoptionボタンのコードをかく
+});
