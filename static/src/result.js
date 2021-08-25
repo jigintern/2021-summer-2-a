250fd29b40a482e9ec7e9c9d84c38692e29cb4bb
i18sawada <i18sawada@tokuyama.kosen-ac.jp>
2021-08-25 10:08:12 +0900

    reslut画面からのページ移動

---
+++ static/src/result.js
@@ -0,0 +1,10 @@
+
+let top = document.getElementById("top");
+let main = document.getElementById("main");
+
+top.addEventListener("click", () => {
+  location.href = "../top.html";
+}, false);
+main.addEventListener("click", () => {
+  location.href = "../main.html";
+});
