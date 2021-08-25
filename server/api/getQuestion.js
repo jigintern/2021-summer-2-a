6a60014bdb393887e69a023fe1226f722cb13cd6
silvietf <silwekis3095@gmail.com>
2021-08-25 13:57:15 +0900

    getQuestion.jsが完成

--- server/api/getQuestion.js
+++ server/api/getQuestion.js
@@ -4,22 +4,20 @@ import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
  * @param {int} num  欲しい件数の番号データ件数
  */
 export function getQuestion(num) {
-    
     const json =new JSONDB("./server/json/questions.json");
-    // TODO 下のエラー処理は後で書く。
-    // if (num>post.quizData.read().length) {
-    //     //numがjSONの長さより大きすぎたとき、
-    //     return "Error: 件数の入力が問題データ件数を超えている。"
-    // }
     let shuffle_question=new Array(num);
-    let a=0;
     /***シャッフル***/
     for (let i = 0; i < num; i++) {
-        a=Math.floor(Math.random()*num);
-        shuffle_question[i]=json.data.quizData[Math.floor(Math.random()*num)];
+        shuffle_question[i]=json.data.quizData[Math.floor(Math.random()*json.data.quizData.length)];
     }
     /****************/
+    // 問題から回答のみ削除
+    const get_question = shuffle_question.map((data) => {
+        delete data.answersId;
+        delete data.explanation;
+        return data;
+    });
 
-    return shuffle_question; 
+    return get_question; 
 }
 
