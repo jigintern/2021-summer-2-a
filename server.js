a1f32c896c5acad6fd663b5a478f660ce359d674
nosk <liath.fail.org@gmail.com>
2021-08-25 09:49:58 +0900

    Getのテストとか

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import { Server } from "https://js.sabae.cc/Server.js";
+import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
+import { fetchJSON } from 'https://js.sabae.cc/fetchJSON.js';
 
 class QuizServer extends Server {
     api(path, req) {
-        if (path == "/api/test") {
-            return "test"
+        switch(path) {
+            case "/api/tests" :
+                console.log(req);
+                const reqJson = JSON.parse('{"' + decodeURI(req.replace(/&/g, "\",\"").replace(/=/g, "\":\"")) + '"}');
+                console.log(reqJson);
+                return reqJson;
+            case "/api/questions":
+               const res = new JSONDB("json/questions.json");
+               return res.data;
         }
+        
     }
 }
 new QuizServer(8001);
