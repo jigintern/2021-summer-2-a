b377e916b21d2f15d83dbd914078302d9117e657
silvietf <silwekis3095@gmail.com>
2021-08-25 13:35:33 +0900

    getQuestion.jsを作成。

--- server/api/getQuestion.js
+++ server/api/getQuestion.js
@@ -1,25 +1,25 @@
 import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
 import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
-
 /**
  * @param {int} num  欲しい件数の番号データ件数
  */
-const POSTquestions =  (num) => {
-    const post=new JSONDB("../json/questions.json");
+export function getQuestion(num) {
+    
+    const json =new JSONDB("./server/json/questions.json");
     // TODO 下のエラー処理は後で書く。
     // if (num>post.quizData.read().length) {
     //     //numがjSONの長さより大きすぎたとき、
     //     return "Error: 件数の入力が問題データ件数を超えている。"
     // }
     let shuffle_question=new Array(num);
+    let a=0;
     /***シャッフル***/
     for (let i = 0; i < num; i++) {
-        shuffle_question[i]=post.quizData[Math.floor(Math.random()*num)];
+        a=Math.floor(Math.random()*num);
+        shuffle_question[i]=json.data.quizData[Math.floor(Math.random()*num)];
     }
     /****************/
 
     return shuffle_question; 
-    // console.log(shuffle_question);
 }
 
-export { POSTquestions }
