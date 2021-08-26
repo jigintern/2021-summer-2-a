d57a2ffa55e548a2a5a5f5a2bb9b4b45aeaba4df
okomechen <tomoaki.1023.8686@gmail.com>
2021-08-26 14:56:40 +0900

    session_idをindex.htmlで生成した

--- static/src/index.js
+++ static/src/index.js
@@ -1,8 +1,29 @@
+import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js"
+
+let session_id=localStorage.getItem("session_id");
+console.log(session_id)
+session_id=await fetchJSON("/api/getSessionId",{session:session_id});
+session_id=String(session_id);
+localStorage.setItem("session_id",session_id);
+console.log(session_id);
+
 let start = document.getElementById("start");
 let option = document.getElementById("option");
+let input =document.getElementById("inputNickname")
 start.addEventListener("click", () => {
   location.href = "../main.html";
 }, false);
 option.addEventListener("click", () => {
   location.href = "../option.html";
 });
+
+function changeNickname() {
+  console.log("inputButton was pushed");
+  const nickname = document.getElementById("recentNickname");
+  let input = document.getElementById("inputNickname");
+
+  if (input.value == "") {
+    sampleArea.innerHTML = "名前が入力されていません";
+  } else {
+  }
+}
