b6a858349a8631c8e3a52cfb8fe6fc33209da089
okomechen <tomoaki.1023.8686@gmail.com>
2021-08-26 16:51:20 +0900

    add userName

--- static/src/index.js
+++ static/src/index.js
@@ -1,16 +1,27 @@
-import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js"
+import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
 
-let session_id=localStorage.getItem("session_id");
-console.log(session_id)
-session_id=await fetchJSON("/api/getSessionId",{session:session_id});
-session_id=String(session_id);
-localStorage.setItem("session_id",session_id);
+let session_id = localStorage.getItem("session_id");
+console.log(session_id);
+session_id = await fetchJSON("/api/getSessionId", { session: session_id });
+session_id = String(session_id);
+localStorage.setItem("session_id", session_id);
 console.log(session_id);
 
 let start = document.getElementById("start");
 let option = document.getElementById("option");
-let input =document.getElementById("inputNickname")
+let input = document.getElementById("inputNickname");
 
+start.onclick = async () => {
+  if (input.value == "") {
+    console.log("no nickname");
+  } else {
+    await fetchJSON("/api/saveUserName", {
+      name: input.value,
+      session: session_id,
+    });
+  }
+  location.href = "./main.html";
+};
 
 function changeNickname() {
   console.log("inputButton was pushed");
@@ -20,13 +31,5 @@ function changeNickname() {
   if (input.value == "") {
     sampleArea.innerHTML = "名前が入力されていません";
   } else {
-  }
-}
-function start(){
-  let input = document.getElementById("inputNickname");
-  if(input.value==""){
-
-  }else{
-    
   }
 }
