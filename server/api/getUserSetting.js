f7703f9ff5058cd752fa2cb262836764331d9eb9
silvietf <silwekis3095@gmail.com>
2021-08-26 17:37:04 +0900

    getUserSetting.jsにコメントを追加。

--- server/api/getUserSetting.js
+++ server/api/getUserSetting.js
@@ -1,6 +1,14 @@
 import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
 const save_json = new JSONDB("./server/json/user_setting.json");
 
+/**
+ * ユーザーIDからIDと問題数を返す関数。
+ * 
+ * 引数
+ * @argument {string} json ユーザーIDの文字列
+ * 返り値
+ * @param {json} {"sessionId":,"question_volume":} セッションIDと問題数
+ */
 //入力はsessionId
 //出力はsessionIdと問題数
 export function getUserSetting(json) {
