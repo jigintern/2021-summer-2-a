0d27bf0c5001978c60328c1adcb45687e9a06f21
i18sawada <i18sawada@tokuyama.kosen-ac.jp>
2021-08-26 17:30:36 +0900

    api/getUserSetting を作成

--- server/api/getUserSetting.js
+++ server/api/getUserSetting.js
@@ -1,8 +1,16 @@
 import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
-//入力：sessionID
-//出力：ユーザーの設定を返す。{"sessionId":"025110","question_volume":10}
+const save_json = new JSONDB("./server/json/user_setting.json");
+
+//入力はsessionId
+//出力はsessionIdと問題数
 export function getUserSetting(json) {
-    const save_json=new JSONDB("./server/json/user_setting.json");
-    let check=save_json.data.user_setting.find(ele=>ele.sessionId==json.sessionId)
-    let index=save_json.data.user_setting.indexOf(check);
+    let check = save_json.data.user_setting.find(ele=>ele.sessionId);
+    let index = save_json.data.user_setting.indexOf(check);
+    console.log("あいう");
+    console.log(index);
+
+    return {
+        sessionId: json,
+        question_volume: save_json.data.user_setting[index].question_volume
+    }
 }
