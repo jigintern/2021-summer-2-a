d79fb4e0d58518e1513c58da6ab78700b1233647
okomechen <tomoaki.1023.8686@gmail.com>
2021-08-27 11:11:13 +0900

    update main.js

--- static/src/main.js
+++ static/src/main.js
@@ -1,11 +1,7 @@
 import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
-import {
-  removeElement,
-  getElement,
-  getSessionId,
-} from './utils/util.js'
+import { removeElement, getElement, getSessionId } from "./utils/util.js";
 
-const FINAL_QUIZ_ID = 99999
+const FINAL_QUIZ_ID = 99999;
 
 let state = new Map();
 
@@ -14,111 +10,114 @@ const setQuizList = (data) => {
     if (i !== data.length - 1) {
       a.nextId = data[i + 1]["quizId"];
     } else {
-      a.nextId = FINAL_QUIZ_ID
+      a.nextId = FINAL_QUIZ_ID;
     }
     return a;
   });
   state.set("quizList", data);
 };
+getElement("submitButton").disabled = true;
 
 const getQuizList = () => state.get("quizList");
 const setCurrentChoice = (data) => state.set("currentChoice", data);
-const getCurrentChoice = () => state.get("currentChoice");
+const getCurrentChoice = () => {
+  state.get("currentChoice");
+};
 const setCurrentQuiz = (data) => state.set("currentQuiz", data);
 const getCurrentQuiz = () => state.get("currentQuiz");
 const setAnswers = ({ id, currAns }) => {
   const newAnswers = getAnswers().map((d) => {
-    return d.quizId === id
-      ? { quizId: d.quizId, amswer: currAns }
-      : d
-  })
-  state.set("answers", newAnswers)
-}
-const getAnswers = () => state.get("answers")
-const initAnsList = () => state.set(
-  "answers",
-  getQuizList().map(({ quizId }) => ({ quizId, answer: 999 }))
-)
+    return d.quizId === id ? { quizId: d.quizId, amswer: currAns } : d;
+  });
+  state.set("answers", newAnswers);
+};
+const getAnswers = () => state.get("answers");
+const initAnsList = () =>
+  state.set(
+    "answers",
+    getQuizList().map(({ quizId }) => ({ quizId, answer: 999 }))
+  );
 
 const createChoices = ({ choices }) => {
   choices.map(({ text, id }) => {
-    const container = document.createElement("div")
+    const container = document.createElement("div");
     const choiceBtn = document.createElement("button");
     const crrElId = `choices_${id}`;
 
     choiceBtn.setAttribute("id", crrElId);
     choiceBtn.setAttribute("class", "main-choice-button button_unclick");
     choiceBtn.innerText = text;
-    container.appendChild(choiceBtn)
+    container.appendChild(choiceBtn);
     getElement("choicesContainer").appendChild(container);
 
     getElement(crrElId).onclick = () => {
+      getElement("submitButton").disabled = false;
       setCurrentChoice(id);
-    }
+    };
   });
 
   return;
 };
 
 const end = async () => {
-  const nowSession = getSessionId()
-  const answers = getAnswers()
+  const nowSession = getSessionId();
+  const answers = getAnswers();
   const a = await fetchJSON("/api/saveAnswer", {
     session: nowSession,
-    answers: answers
-  })
-  console.log(a)
-}
+    answers: answers,
+  });
+  console.log(a);
+};
 
 window.onload = async () => {
-  const nowSession = getSessionId()
+  const nowSession = getSessionId();
   if (!nowSession) {
-    location.href = "/"
+    location.href = "/";
   }
   getElement("exit").onclick = () => {
-    location.href = '/'
-  }
+    location.href = "/";
+  };
   setQuizList(await fetchJSON("/api/getQuestion", { session: nowSession }));
-  initAnsList(initAnsList)
-  setCurrentQuiz(getQuizList()[0])
-  loopQuiz(getCurrentQuiz())
+  initAnsList(initAnsList);
+  setCurrentQuiz(getQuizList()[0]);
+  loopQuiz(getCurrentQuiz());
 };
 
 const loopQuiz = async (nowQuiz) => {
-  getElement("quizStatement").innerText = nowQuiz.statement
+  getElement("quizStatement").innerText = nowQuiz.statement;
   createChoices(nowQuiz);
 
-  visibility("answerContainer", false)
-  visibility("choicesContainer", true)
-  visibility("submitButton", true)
-  visibility("nextButton", false)
-  visibility("resultButton", false)
+  visibility("answerContainer", false);
+  visibility("choicesContainer", true);
+  visibility("submitButton", true);
+  visibility("nextButton", false);
+  visibility("resultButton", false);
 
   getElement("submitButton").onclick = async () => {
     const { nextId } = nowQuiz;
-    visibility("answerContainer", true)
-    visibility("choicesContainer", false)
-    visibility("submitButton", false)
-    
+    visibility("answerContainer", true);
+    visibility("choicesContainer", false);
+    visibility("submitButton", false);
 
     if (nextId === FINAL_QUIZ_ID) {
-      visibility("resultButton", true)
+      visibility("resultButton", true);
     } else {
-      visibility("nextButton", true)
+      visibility("nextButton", true);
     }
 
     setAnswers({
       id: nowQuiz.quizId,
       currAns: getCurrentChoice(),
-    })
+    });
 
     const { explanation, answerId } = await fetchJSON("/api/getAnswer", {
-      quizId: nowQuiz.quizId
-    })
+      quizId: nowQuiz.quizId,
+    });
 
-    const choiceText = nowQuiz.choices.find(a => a.id === answerId).text
-    getElement("explanation").innerText = explanation
-    getElement("answer").innerText = choiceText
+    const choiceText = nowQuiz.choices.find((a) => a.id === answerId).text;
+    getElement("explanation").innerText = explanation;
+    getElement("answer").innerText = choiceText;
+    console.log(getCurrentChoice());
   };
 
   getElement("nextButton").onclick = async () => {
@@ -126,22 +125,23 @@ const loopQuiz = async (nowQuiz) => {
     const nextQuiz = getQuizList().find((a) => a.quizId === nextId);
 
     removeElement("#choicesContainer");
-    visibility("submitButton", false)
-    visibility("nextButton", true)
-    
-    getElement("quizStatement").innerText = ""
-    getElement("explanation").innerText = ""
-    getElement("answer").innerText = ""
+    visibility("submitButton", false);
+    visibility("nextButton", true);
+
+    getElement("quizStatement").innerText = "";
+    getElement("explanation").innerText = "";
+    getElement("answer").innerText = "";
     loopQuiz(nextQuiz);
-  }
+    getElement("submitButton").disabled = true;
+  };
 
   getElement("resultButton").onclick = () => {
-    location.href = "/result.html"
-  }
+    location.href = "/result.html";
+  };
 };
 
 const visibility = (el, isVisible) => {
   isVisible
-    ? getElement(el).style.display = "block"
-    : getElement(el).style.display = "none"
-}
+    ? (getElement(el).style.display = "block")
+    : (getElement(el).style.display = "none");
+};
