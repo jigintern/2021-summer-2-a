52d11e4f7f053ce9a088ad5a9a88a40cfa972966
nosk <liath.fail.org@gmail.com>
2021-08-27 15:57:49 +0900

    らすとこみっっっっとおおお

--- static/src/utils/util.js
+++ static/src/utils/util.js
@@ -1,13 +1,16 @@
+import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
+
 const removeElement = (selector) => {
   document.querySelector(selector).innerHTML = "";
 };
 
 const getElement = (id) => document.getElementById(id);
 
-const getSessionId = () => {
-  const nowSession = localStorage.getItem("session_id");
+const getSessionId = async () => {
+  let nowSession = localStorage.getItem("session_id");
   if (!nowSession) {
-      location.href='./index.html';
+      nowSession = await fetchJSON("/api/getSessionId", { session: nowSession });
+      // location.href='./index.html';
   }
   return nowSession;
 };
