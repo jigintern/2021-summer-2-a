a1cbb6a8548a2e56708df25d7680975b6f045dab
nosk <liath.fail.org@gmail.com>
2021-08-27 16:00:10 +0900

    Revert "らすとこみっっっっとおおお"
    This reverts commit 52d11e4f7f053ce9a088ad5a9a88a40cfa972966.

--- static/src/utils/util.js
+++ static/src/utils/util.js
@@ -1,16 +1,13 @@
-import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
-
 const removeElement = (selector) => {
   document.querySelector(selector).innerHTML = "";
 };
 
 const getElement = (id) => document.getElementById(id);
 
-const getSessionId = async () => {
-  let nowSession = localStorage.getItem("session_id");
+const getSessionId = () => {
+  const nowSession = localStorage.getItem("session_id");
   if (!nowSession) {
-      nowSession = await fetchJSON("/api/getSessionId", { session: nowSession });
-      // location.href='./index.html';
+      location.href='./index.html';
   }
   return nowSession;
 };
